1f0882121843c3342d8b8a652725e188b45e886e
christiandersen <ca@klonk.dk>
2019-11-13 13:41:35 +0100

    Update Log.cs
    Changed namespace name

--- ChristiAndersen.Easylog/source/Log.cs
+++ ChristiAndersen.Easylog/source/Log.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Pvs.EasyflowExtensions.Dispatch.EasyLog
+namespace ChristiAndersen.EasyLog
 {
     public static class Log
     {
